82609876dcbebe42153953fe32560bf739f7b7c8
Bryce Bedwell <brycebedwell@gmail.com>
2016-11-07 16:32:52 -0600

    Added travis configuration

--- gulpfile.js
+++ gulpfile.js
@@ -5,24 +5,24 @@ var useref = require('gulp-useref');
 var uglify = require('gulp-uglify');
 var gulpif = require('gulp-if');
 
-gulp.task('build', ['clean', 'copy', 'install', 'useref'])
+gulp.task('build', ['copy', 'install', 'useref'])
 
 gulp.task('clean', function() {
   return gulp.src('./dist')
     .pipe(clean());
 });
 
-gulp.task('copy', ['clean'], function() {
+gulp.task('copy', function() {
   return gulp.src(['./src/**/*.html', '!./src/index.html', '!./src/bower_components/', '!./src/bower_components/**'])
     .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('install', ['clean'], function() {
+gulp.task('install', function() {
   return gulp.src(['./bower.json'])
     .pipe(install());
 });
 
-gulp.task('useref', ['clean'], function() {
+gulp.task('useref', ['install'], function() {
   return gulp.src('./src/index.html')
     .pipe(useref())
     .pipe(gulpif('*.js', uglify()))
